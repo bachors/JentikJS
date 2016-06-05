49f6dcc16502b3e001ca844bdb2ee6300d018f8e
Ican Bachors <bachor.can@gmail.com>
2016-06-06 00:42:22 +0700

    New add ongkir POS indonesia

--- src/jentik.js
+++ src/jentik.js
@@ -7,7 +7,7 @@
 *****************************************************************************/
 
 var jentik = function() {
-    $('#jentik').html('<div class="tabs"><div class="tab1 aktip1">ongkir</div><div class="tab1">tracking</div></div><div class="tabs"><div class="tab2 aktip2">jne</div><div class="tab2">tiki</div><div class="tab2">pos</div></div><span id="jentik-anu" style="display:none">ongkir</span><div class="kotak"><div id="kotak-ongkir"><div id="kotak-ongkir-tiki" style="display:none" title="Cek Ongkir TIKI"><span class="title">Kota Asal</span><input type="text" id="tiki_dari" list="rute_tiki_dari" placeholder="Bandung"/><datalist id="rute_tiki_dari"></datalist><span class="title">Kota Tujuan</span><input type="text" id="tiki_ke" list="rute_tiki_ke" placeholder="Surabaya"/><datalist id="rute_tiki_ke"></datalist><span class="title">Berat (Kg)</span><input type="text" id="tiki_kg" placeholder="4"/><input type="submit" class="cek_ongkir" data-jentik="tiki" value="Cek Ongkir"/></div><div id="kotak-ongkir-jne" title="Cek Ongkir JNE"><span class="title">Kota Asal</span><input type="text" id="jne_dari" list="rute_jne_dari" placeholder="Bandung"/><datalist id="rute_jne_dari"></datalist><span class="title">Kota Tujuan</span><input type="text" id="jne_ke" list="rute_jne_ke" placeholder="Surabaya"/><datalist id="rute_jne_ke"></datalist><span class="title">Berat (Kg)</span><input type="text" id="jne_kg" placeholder="4"/><input type="submit" class="cek_ongkir" data-jentik="jne" value="Cek Ongkir"/></div><div id="kotak-ongkir-pos" style="display:none" title="Cek Ongkir POS Indonesia"><p align="center">Coming Soon..</p></div></div><div id="kotak-tracking"><div id="kotak-tracking-tiki" style="display:none" title="Cek Tracking TIKI"><span class="title">Nomor Resi</span><input type="text" id="tiki_resi" placeholder="xxx"/><input type="submit" class="cek_resi" data-jentik="tiki" value="Cek Resi"/></div><div id="kotak-tracking-jne" style="display:none" title="Cek Tracking JNE"><span class="title">Nomor Resi</span><input type="text" id="jne_resi" placeholder="xxx"/><input type="submit" class="cek_resi" data-jentik="jne" value="Cek Resi"/></div><div id="kotak-tracking-pos" style="display:none" title="Cek Tracking POS Indonesia"><span class="title">Nomor Resi</span><input type="text" id="pos_resi" placeholder="xxx"/><input type="submit" class="cek_resi" data-jentik="pos" value="Cek Resi"/></div></div></div><div id="jentik-output"></div><div id="footer"><a href="https://github.com/bachors/JentikJS" class="jgf" target="_blank">Made with<3</a></div>');
+    $('#jentik').html('<div class="tabs"><div class="tab1 aktip1">ongkir</div><div class="tab1">tracking</div></div><div class="tabs"><div class="tab2 aktip2">jne</div><div class="tab2">tiki</div><div class="tab2">pos</div></div><span id="jentik-anu" style="display:none">ongkir</span><div class="kotak"><div id="kotak-ongkir"><div id="kotak-ongkir-tiki" style="display:none" title="Cek Ongkir TIKI"><span class="title">Kota Asal</span><input type="text" id="tiki_dari" list="rute_tiki_dari" placeholder="Bandung"/><datalist id="rute_tiki_dari"></datalist><span class="title">Kota Tujuan</span><input type="text" id="tiki_ke" list="rute_tiki_ke" placeholder="Surabaya"/><datalist id="rute_tiki_ke"></datalist><span class="title">Berat (Kg)</span><input type="text" id="tiki_kg" placeholder="4"/><input type="submit" class="cek_ongkir" data-jentik="tiki" value="Cek Ongkir"/></div><div id="kotak-ongkir-jne" title="Cek Ongkir JNE"><span class="title">Kota Asal</span><input type="text" id="jne_dari" list="rute_jne_dari" placeholder="Bandung"/><datalist id="rute_jne_dari"></datalist><span class="title">Kota Tujuan</span><input type="text" id="jne_ke" list="rute_jne_ke" placeholder="Surabaya"/><datalist id="rute_jne_ke"></datalist><span class="title">Berat (Kg)</span><input type="text" id="jne_kg" placeholder="4"/><input type="submit" class="cek_ongkir" data-jentik="jne" value="Cek Ongkir"/></div><div id="kotak-ongkir-pos" style="display:none" title="Cek Ongkir POS Indonesia"><span class="title">Kota Asal</span><input type="text" id="pos_dari" list="rute_pos_dari" placeholder="Bandung"/><datalist id="rute_pos_dari"></datalist><span class="title">Kota Tujuan</span><input type="text" id="pos_ke" list="rute_pos_ke" placeholder="Surabaya"/><datalist id="rute_pos_ke"></datalist><span class="title">Berat (Kg)</span><input type="text" id="pos_kg" placeholder="4"/><input type="submit" class="cek_ongkir" data-jentik="pos" value="Cek Ongkir"/></div></div><div id="kotak-tracking"><div id="kotak-tracking-tiki" style="display:none" title="Cek Tracking TIKI"><span class="title">Nomor Resi</span><input type="text" id="tiki_resi" placeholder="xxx"/><input type="submit" class="cek_resi" data-jentik="tiki" value="Cek Resi"/></div><div id="kotak-tracking-jne" style="display:none" title="Cek Tracking JNE"><span class="title">Nomor Resi</span><input type="text" id="jne_resi" placeholder="xxx"/><input type="submit" class="cek_resi" data-jentik="jne" value="Cek Resi"/></div><div id="kotak-tracking-pos" style="display:none" title="Cek Tracking POS Indonesia"><span class="title">Nomor Resi</span><input type="text" id="pos_resi" placeholder="xxx"/><input type="submit" class="cek_resi" data-jentik="pos" value="Cek Resi"/></div></div></div><div id="jentik-output"></div><div id="footer"><a href="https://github.com/bachors/JentikJS" class="jgf" target="_blank">Made with<3</a></div>');
     $('body').on('click', '#jentik .tab1', function() {
         var a = $(this).html(),
             tab2 = $('#jentik .aktip2').html();
@@ -75,6 +75,30 @@ var jentik = function() {
             }, 50)
         })
     });
+    $('body').on('focus', '#pos_dari', function() {
+        var a = $(this).attr('list');
+        $(this).keydown(function() {
+            setTimeout(function() {
+                var d = $('#pos_dari').val(),
+                    jum = d.length;
+                if (jum > 2) {
+                    rute_pos(d, a)
+                }
+            }, 50)
+        })
+    });
+    $('body').on('focus', '#pos_ke', function() {
+        var a = $(this).attr('list');
+        $(this).keydown(function() {
+            setTimeout(function() {
+                var d = $('#pos_ke').val(),
+                    jum = d.length;
+                if (jum > 2) {
+                    rute_pos(d, a)
+                }
+            }, 50)
+        })
+    });
     $('body').on('focus', '#jne_dari', function() {
         var a = $(this).attr('list');
         $(this).keydown(function() {
@@ -102,9 +126,22 @@ var jentik = function() {
     $('body').on('click', '.cek_ongkir', function() {
         $('#jentik-output').html('<p>Wait..</p>');
         var a = $(this).data('jentik'),
-            dari = (a == 'jne' ? $("#jne_dari").val() : $("#tiki_dari").val()),
-            ke = (a == 'jne' ? $("#jne_ke").val() : $("#tiki_ke").val()),
-            berat = (a == 'jne' ? $("#jne_kg").val() : $("#tiki_kg").val());
+            dari = '',
+            ke = '',
+            berat = '';
+			if(a == 'jne'){
+				dari = $("#jne_dari").val();
+				ke = $("#jne_ke").val();
+				berat = $("#jne_kg").val();
+			}else if(a == 'tiki'){
+				dari = $("#tiki_dari").val();
+				ke = $("#tiki_ke").val();
+				berat = $("#tiki_kg").val();
+			}else if(a == 'pos'){
+				dari = $("#pos_dari").val();
+				ke = $("#pos_ke").val();
+				berat = $("#pos_kg").val();
+			}
         cekongkir(a, dari, ke, berat);
         return false
     });
@@ -126,6 +163,20 @@ var jentik = function() {
     function rute_tiki(d, e) {
         $.ajax({
             url: 'http://ibacor.com/api/rute-jne-tiki?p=tiki&q=' + d,
+            crossDomain: true,
+            dataType: 'json'
+        }).done(function(b) {
+            var c = '';
+            $.each(b, function(i, a) {
+                c += '<option value="' + b[i] + '">'
+            });
+            $('#' + e).html(c)
+        })
+    }
+
+    function rute_pos(d, e) {
+        $.ajax({
+            url: 'http://ibacor.com/api/rute-jne-tiki?p=pos&q=' + d,
             crossDomain: true,
             dataType: 'json'
         }).done(function(b) {
@@ -164,10 +215,13 @@ var jentik = function() {
                 e += '<table>';
                 if (f == 'tiki') {
 					w += 'http://tiki-online.com';
-                    e += '<tr><td>LAYANAN</td><td>TARIF</td></tr>'
+                    e += '<tr><td>LAYANAN</td><td>TARIF</td><td>ETA</td></tr>'
                 } else if (f == 'jne') {
 					w += 'http://www.jne.co.id';
                     e += '<tr><td>LAYANAN</td><td>KIRIMAN</td><td>TARIF</td><td>ETD</td></tr>'
+                } else if (f == 'pos') {
+					w += 'http://www.posindonesia.co.id/';
+                    e += '<tr><td>LAYANAN</td><td>TARIF</td><td>ETD</td></tr>'
                 }
                 $.each(d.ongkos, function(i, c) {
                     e += '<tr>';
